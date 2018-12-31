b3f31e9985e167826e87c0f8eca45e9e3c2db096
Daniel Leech <dan.t.leech@gmail.com>
2018-12-31 17:51:13 +0000

    HoverTextProvider interface uses TextDocument API

--- lib/Core/HoverTextProvider.php
+++ lib/Core/HoverTextProvider.php
@@ -2,7 +2,10 @@
 
 namespace Phpactor\Completion\Core;
 
+use Phpactor\TextDocument\ByteOffset;
+use Phpactor\TextDocument\TextDocument;
+
 interface HoverTextProvider
 {
-    public function hover(string $source, int $byteOffset): HoverText;
+    public function hover(TextDocument $source, ByteOffset $byteOffset): HoverText;
 }
