9f8b468be0cb09664884ad434a722a509aeab091
Daniel Leech <dan.t.leech@gmail.com>
2020-04-04 12:26:20 +0100

    Add PHPUnit < 8 stubs
    As Phpactor will test this package with PHPUnit 8 currently

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,13 +1,30 @@
 <?php
 
-namespace Phpactor\Completion\Tests;
+// add stubs for PHPUnit < 8
+// when all packages are using PHPUnit 9 these can be removed
 
-use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
-use PHPUnit\Framework\TestCase as PhpUnitTestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
+namespace DMS\PHPUnitExtensions\ArraySubset{
+    if (!class_exists(ArraySubsetAsserts::class)) {
+        trait ArraySubsetAsserts {}
+    }
+}
 
-class TestCase extends PhpUnitTestCase
-{
-    use ArraySubsetAsserts;
-    use ProphecyTrait;
+namespace Prophecy\PhpUnit {
+    if (!class_exists(ProphecyTrait::class)) {
+        trait ProphecyTrait {}
+    }
+}
+
+namespace Phpactor\Completion\Tests {
+
+    use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+    use PHPUnit\Framework\TestCase as PhpUnitTestCase;
+    use Prophecy\PhpUnit\ProphecyTrait;
+
+
+    class TestCase extends PhpUnitTestCase
+    {
+        use ArraySubsetAsserts;
+        use ProphecyTrait;
+    }
 }
