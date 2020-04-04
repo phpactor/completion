43fe263d1a345379ce6647aaa444fc99440d3e8f
Daniel Leech <dan.t.leech@gmail.com>
2020-04-04 09:20:54 +0100

    Adds RPC extension

---
+++ lib/Extension/CompletionRpcExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Phpactor\Completion\Extension;
+
+use Phpactor\Container\Container;
+use Phpactor\Container\ContainerBuilder;
+use Phpactor\Container\Extension;
+use Phpactor\Completion\Extension\Handler\CompleteHandler;
+use Phpactor\Extension\Completion\CompletionExtension;
+use Phpactor\Extension\Rpc\RpcExtension;
+use Phpactor\MapResolver\Resolver;
+
+class CompletionRpcExtension implements Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function configure(Resolver $schema)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ContainerBuilder $container)
+    {
+        $container->register('completion_rpc.handler', function (Container $container) {
+            return new CompleteHandler($container->get(CompletionExtension::SERVICE_REGISTRY));
+        }, [ RpcExtension::TAG_RPC_HANDLER => ['name' => CompleteHandler::NAME] ]);
+    }
+}
