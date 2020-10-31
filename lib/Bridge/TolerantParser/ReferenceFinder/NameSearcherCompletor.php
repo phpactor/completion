dfe14bb0c069adc38dd5e727c4149d9a2253fb39
Camille Dejoye <camilledejoye@gmail.com>
2020-10-31 16:48:46 +0100

    review: remove useless constructor

--- lib/Bridge/TolerantParser/ReferenceFinder/NameSearcherCompletor.php
+++ lib/Bridge/TolerantParser/ReferenceFinder/NameSearcherCompletor.php
@@ -6,17 +6,11 @@ use Generator;
 use Microsoft\PhpParser\Node;
 use Phpactor\Completion\Bridge\TolerantParser\TolerantCompletor;
 use Phpactor\Completion\Core\Completor\NameSearcherCompletor as CoreNameSearcherCompletor;
-use Phpactor\ReferenceFinder\NameSearcher;
 use Phpactor\TextDocument\ByteOffset;
 use Phpactor\TextDocument\TextDocument;
 
 class NameSearcherCompletor extends CoreNameSearcherCompletor implements TolerantCompletor
 {
-    public function __construct(NameSearcher $nameSearcher)
-    {
-        parent::__construct($nameSearcher);
-    }
-
     /**
      * {@inheritDoc}
      */
