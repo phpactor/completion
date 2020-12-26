fe296af68e71d08caf4b76b41ec72a7332905b01
Vladislav Kosev <vladislav.kosev@outlook.com>
2020-12-26 20:33:39 +0200

    Deal with keyword completions in: - inside class member list - when accessing member properties, methods and constants - inside strings - after anonymous function braces before return statement

--- lib/Bridge/TolerantParser/WorseReflection/KeywordCompletor.php
+++ lib/Bridge/TolerantParser/WorseReflection/KeywordCompletor.php
@@ -6,8 +6,13 @@ namespace Phpactor\Completion\Bridge\TolerantParser\WorseReflection;
 
 use Generator;
 use Microsoft\PhpParser\Node;
+use Microsoft\PhpParser\Node\ClassMembersNode;
+use Microsoft\PhpParser\Node\Expression\AnonymousFunctionCreationExpression;
 use Microsoft\PhpParser\Node\Expression\MemberAccessExpression;
-use Microsoft\PhpParser\Node\Statement\ClassDeclaration;
+use Microsoft\PhpParser\Node\Expression\ScopedPropertyAccessExpression;
+use Microsoft\PhpParser\Node\Expression\Variable;
+use Microsoft\PhpParser\Node\Statement\CompoundStatementNode;
+use Microsoft\PhpParser\Node\StringLiteral;
 use Microsoft\PhpParser\TokenStringMaps;
 use Phpactor\Completion\Bridge\TolerantParser\TolerantCompletor;
 use Phpactor\Completion\Core\Suggestion;
@@ -17,34 +22,71 @@ use function get_class;
 
 class KeywordCompletor implements TolerantCompletor
 {
+    public const MEMBER_ACCESS = "member_access";
+    public const CLASS_MEMBERS = "class_members";
+    public const STRING_LITERAL = "string_literal";
+    public const VARIABLE = "variable";
+    public const AFTER_ANONYMOUS_FUNC_PARAMS = "after_anonymous_func_params";
+
+    public const SPECIAL_SCOPES = [
+        self::MEMBER_ACCESS => [ ],
+        self::STRING_LITERAL => [ ],
+        self::VARIABLE => [ ],
+        // https://github.com/php/php-langspec/blob/master/spec/10-expressions.md#anonymous-function-creation
+        self::AFTER_ANONYMOUS_FUNC_PARAMS => [ 
+            'use'
+        ],
+        // https://github.com/php/php-langspec/blob/master/spec/14-classes.md#grammar-class-member-declaration
+        self::CLASS_MEMBERS => [
+            // visibility
+            'public',
+            'private',
+            'protected',
+            // scope
+            'static',
+            // property
+            'var',
+            // const
+            'const',
+            // method
+            'function',
+            // trait use
+            'use',
+        ],
+    ];
+
+    private $allKeywords;
+
+    public function __construct()
+    {
+        $this->allKeywords = array_merge(
+            array_keys(TokenStringMaps::RESERVED_WORDS),
+            array_keys(TokenStringMaps::KEYWORDS)
+        );
+    }
     /**
     * {@inheritDoc}
     */
     public function complete(Node $node, TextDocument $source, ByteOffset $offset): Generator
     {
+        $scopeName = $this->getScopeName($node, $offset);
+        
         $parents = [
             get_class($node)
         ];
         $parent = $node;
-        while(($parent = $parent->getParent()) !== null){
+        while (($parent = $parent->getParent()) !== null) {
             $parents[] = get_class($parent);
         }
 
         dump(implode("\n    ->", $parents));
-        
-        if($node->getParent() instanceof ClassDeclaration){
-            /** @var ClassDeclaration $declaration */
-            $declaration = $node->getParent();
-            $nameEnd = $declaration->name->getEndPosition();
-            // dump("name end: {$nameEnd}, offset: {$offset->toInt()}");
-        } else if($node instanceof MemberAccessExpression){
-            /** @var MemberAccessExpression $memberAccess */
-            $memberAccess = $node;
-            $nameEnd = $memberAccess->memberName->getEndPosition();
-            if($nameEnd == $offset->toInt())
-                return true;
-        }
-        foreach (array_merge(array_keys(TokenStringMaps::RESERVED_WORDS), array_keys(TokenStringMaps::KEYWORDS)) as $keyword) {
+
+        $keywords =
+            (isset(self::SPECIAL_SCOPES[$scopeName])) ?
+                self::SPECIAL_SCOPES[$scopeName] :
+                $this->allKeywords;
+
+        foreach ($keywords as $keyword) {
             yield Suggestion::createWithOptions(
                 $keyword,
                 [
@@ -53,5 +95,37 @@ class KeywordCompletor implements TolerantCompletor
             );
         }
         return true;
+    }
+
+    private function getScopeName(Node $node, ByteOffset $offset): ?string
+    {
+        $scopeName = null;
+        if (
+            ($node instanceof MemberAccessExpression || $node instanceof ScopedPropertyAccessExpression)
+            && ($node->memberName->getEndPosition() == $offset->toInt())
+        ) {
+            $scopeName = self::MEMBER_ACCESS;
+        } elseif ($node instanceof ClassMembersNode) {
+            $scopeName = self::CLASS_MEMBERS;
+        } elseif ($node instanceof StringLiteral) {
+            $scopeName = self::STRING_LITERAL;
+        } elseif (
+            $node instanceof Variable
+            && ($node->name->getEndPosition() == $offset->toInt())
+        ) {
+            $scopeName = self::VARIABLE;
+        } elseif (
+            $node instanceof CompoundStatementNode
+            && $node->getParent() instanceof AnonymousFunctionCreationExpression
+            && $node->getParent()->closeParen->getEndPosition() == ($offset->toInt() - 1)
+            && (
+                $node->getParent()->colonToken == null
+                || $node->getParent()->colonToken->getStartPosition() > $offset->toInt()
+            )
+        ){
+            $scopeName = self::AFTER_ANONYMOUS_FUNC_PARAMS;
+        }
+
+        return $scopeName;
     }
 }
--- tests/Integration/Bridge/TolerantParser/WorseReflection/KeywordCompletorTest.php
+++ tests/Integration/Bridge/TolerantParser/WorseReflection/KeywordCompletorTest.php
@@ -9,7 +9,6 @@ use Phpactor\Completion\Bridge\TolerantParser\WorseReflection\KeywordCompletor;
 use Phpactor\Completion\Core\Suggestion;
 use Phpactor\Completion\Tests\Integration\Bridge\TolerantParser\TolerantCompletorTestCase;
 use Phpactor\TextDocument\TextDocument;
-use function func_get_arg;
 
 class KeywordCompletorTest extends TolerantCompletorTestCase
 {
@@ -18,35 +17,91 @@ class KeywordCompletorTest extends TolerantCompletorTestCase
         return new KeywordCompletor();
     }
     /**
-	 * @dataProvider provideComplete
+     * @dataProvider provideComplete
      */
     public function testComplete(string $source, array $expected): void
     {
         $this->assertComplete($source, $expected);
-	}
-	
-	public function provideComplete(): Generator
-	{
-		$allKeywords = [];
+    }
+    
+    public function provideComplete(): Generator
+    {
         $keywords = array_merge(array_keys(TokenStringMaps::RESERVED_WORDS), array_keys(TokenStringMaps::KEYWORDS));
-        sort($keywords);
-        foreach ($keywords as $keyword) {
-            $allKeywords[] = [
+        $allKeywords = $this->createExpectedKeywords($keywords);
+
+        yield 'all keywords' => [
+            '<?php <>',
+            $allKeywords
+        ];
+        yield 'member' => [
+            '<?php class C { <>',
+            $this->createExpectedKeywords(KeywordCompletor::SPECIAL_SCOPES[KeywordCompletor::CLASS_MEMBERS])
+        ];
+        yield 'member access' => [
+            '<?php function F(){ $v-><>',
+            $this->createExpectedKeywords(KeywordCompletor::SPECIAL_SCOPES[KeywordCompletor::MEMBER_ACCESS])
+        ];
+        yield 'member access with partial name' => [
+            '<?php function F(){ $v->p<>',
+            $this->createExpectedKeywords(KeywordCompletor::SPECIAL_SCOPES[KeywordCompletor::MEMBER_ACCESS])
+        ];
+        yield 'member access after name' => [
+            '<?php function F(){ $v->p <>',
+            $allKeywords
+        ];
+        yield 'scoped member access' => [
+            '<?php function F(){ joe::<>',
+            $this->createExpectedKeywords(KeywordCompletor::SPECIAL_SCOPES[KeywordCompletor::MEMBER_ACCESS])
+        ];
+        yield 'scoped member access with partial name' => [
+            '<?php function F(){ joe::me<>',
+            $this->createExpectedKeywords(KeywordCompletor::SPECIAL_SCOPES[KeywordCompletor::MEMBER_ACCESS])
+        ];
+        yield 'scoped member access after name' => [
+            '<?php function F(){ joe::me <>',
+            $allKeywords
+        ];
+        yield 'scoped member access after name and brace' => [
+            '<?php function F(){ joe::me(<>',
+            $allKeywords
+        ];
+        yield 'inside string' => [
+            '<?php $var ="<> ',
+            $this->createExpectedKeywords(KeywordCompletor::SPECIAL_SCOPES[KeywordCompletor::STRING_LITERAL])
+        ];
+        yield 'inside string expression' => [
+            '<?php $var ="{$var->n<> ',
+            $this->createExpectedKeywords(KeywordCompletor::SPECIAL_SCOPES[KeywordCompletor::MEMBER_ACCESS])
+        ];
+        yield 'var' => [
+            '<?php $var<> ',
+            $this->createExpectedKeywords(KeywordCompletor::SPECIAL_SCOPES[KeywordCompletor::VARIABLE])
+        ];
+        yield 'after var' => [
+            '<?php $var <> ',
+            $allKeywords
+        ];
+        yield 'after function braces' => [
+            '<?php $func = function() <> ',
+            $this->createExpectedKeywords(KeywordCompletor::SPECIAL_SCOPES[KeywordCompletor::AFTER_ANONYMOUS_FUNC_PARAMS])
+        ];
+        yield 'after function braces before return statement' => [
+            '<?php $func = function() <> :string ',
+            $this->createExpectedKeywords(KeywordCompletor::SPECIAL_SCOPES[KeywordCompletor::AFTER_ANONYMOUS_FUNC_PARAMS])
+        ];
+        
+    }
+
+    private function createExpectedKeywords(array $list): array
+    {
+        sort($list);
+        $keywords = [];
+        foreach ($list as $keyword) {
+            $keywords[] = [
                 'type' => Suggestion::TYPE_KEYWORD,
                 'name' => $keyword,
             ];
-		}
-		
-		yield 'all keywords' => [
-			'<?php <>',
-			$allKeywords
-        ];
-        
-        yield 'member access' => [
-			'<?php function F(){ $v-><>',
-			[]
-		];
-		
-		return true;
-	}
+        }
+        return $keywords;
+    }
 }
