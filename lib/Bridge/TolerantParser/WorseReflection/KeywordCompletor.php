ae18713b5d65f95ddb3495346395006ed22e5e48
Vladislav Kosev <vladislav.kosev@outlook.com>
2020-12-26 18:05:40 +0200

    Disable keyword completion on the end of a MemberAccess node

--- lib/Bridge/TolerantParser/WorseReflection/KeywordCompletor.php
+++ lib/Bridge/TolerantParser/WorseReflection/KeywordCompletor.php
@@ -6,11 +6,14 @@ namespace Phpactor\Completion\Bridge\TolerantParser\WorseReflection;
 
 use Generator;
 use Microsoft\PhpParser\Node;
+use Microsoft\PhpParser\Node\Expression\MemberAccessExpression;
+use Microsoft\PhpParser\Node\Statement\ClassDeclaration;
 use Microsoft\PhpParser\TokenStringMaps;
 use Phpactor\Completion\Bridge\TolerantParser\TolerantCompletor;
 use Phpactor\Completion\Core\Suggestion;
 use Phpactor\TextDocument\ByteOffset;
 use Phpactor\TextDocument\TextDocument;
+use function get_class;
 
 class KeywordCompletor implements TolerantCompletor
 {
@@ -19,6 +22,28 @@ class KeywordCompletor implements TolerantCompletor
     */
     public function complete(Node $node, TextDocument $source, ByteOffset $offset): Generator
     {
+        $parents = [
+            get_class($node)
+        ];
+        $parent = $node;
+        while(($parent = $parent->getParent()) !== null){
+            $parents[] = get_class($parent);
+        }
+
+        dump(implode("\n    ->", $parents));
+        
+        if($node->getParent() instanceof ClassDeclaration){
+            /** @var ClassDeclaration $declaration */
+            $declaration = $node->getParent();
+            $nameEnd = $declaration->name->getEndPosition();
+            // dump("name end: {$nameEnd}, offset: {$offset->toInt()}");
+        } else if($node instanceof MemberAccessExpression){
+            /** @var MemberAccessExpression $memberAccess */
+            $memberAccess = $node;
+            $nameEnd = $memberAccess->memberName->getEndPosition();
+            if($nameEnd == $offset->toInt())
+                return true;
+        }
         foreach (array_merge(array_keys(TokenStringMaps::RESERVED_WORDS), array_keys(TokenStringMaps::KEYWORDS)) as $keyword) {
             yield Suggestion::createWithOptions(
                 $keyword,
--- tests/Integration/Bridge/TolerantParser/WorseReflection/KeywordCompletorTest.php
+++ tests/Integration/Bridge/TolerantParser/WorseReflection/KeywordCompletorTest.php
@@ -2,33 +2,51 @@
 
 namespace Phpactor\Completion\Tests\Integration\Bridge\TolerantParser\WorseReflection;
 
+use Generator;
 use Microsoft\PhpParser\TokenStringMaps;
 use Phpactor\Completion\Bridge\TolerantParser\TolerantCompletor;
 use Phpactor\Completion\Bridge\TolerantParser\WorseReflection\KeywordCompletor;
 use Phpactor\Completion\Core\Suggestion;
 use Phpactor\Completion\Tests\Integration\Bridge\TolerantParser\TolerantCompletorTestCase;
 use Phpactor\TextDocument\TextDocument;
+use function func_get_arg;
 
 class KeywordCompletorTest extends TolerantCompletorTestCase
 {
     protected function createTolerantCompletor(TextDocument $source): TolerantCompletor
     {
-        return new KeywordCompletor(
-        );
+        return new KeywordCompletor();
     }
-    
-    public function testComplete(): void
+    /**
+	 * @dataProvider provideComplete
+     */
+    public function testComplete(string $source, array $expected): void
     {
-        $expected = [];
+        $this->assertComplete($source, $expected);
+	}
+	
+	public function provideComplete(): Generator
+	{
+		$allKeywords = [];
         $keywords = array_merge(array_keys(TokenStringMaps::RESERVED_WORDS), array_keys(TokenStringMaps::KEYWORDS));
         sort($keywords);
         foreach ($keywords as $keyword) {
-            $expected[] = [
+            $allKeywords[] = [
                 'type' => Suggestion::TYPE_KEYWORD,
                 'name' => $keyword,
             ];
-        }
-
-        $this->assertComplete("<?php <>", $expected);
-    }
+		}
+		
+		yield 'all keywords' => [
+			'<?php <>',
+			$allKeywords
+        ];
+        
+        yield 'member access' => [
+			'<?php function F(){ $v-><>',
+			[]
+		];
+		
+		return true;
+	}
 }
