832da2abe009dd92a5402ddaae791e3d43c1c02b
Daniel Leech <dan.t.leech@gmail.com>
2020-12-27 23:38:44 +0000

    Add named parameter completor

---
+++ tests/Integration/Bridge/TolerantParser/WorseReflection/WorseNamedParameterCompletorTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Phpactor\Completion\Tests\Integration\Bridge\TolerantParser\WorseReflection;
+
+use Phpactor\Completion\Bridge\TolerantParser\TolerantCompletor;
+use Phpactor\Completion\Bridge\TolerantParser\WorseReflection\WorseNamedParameterCompletor;
+use Phpactor\Completion\Core\Suggestion;
+use Phpactor\Completion\Tests\Integration\Bridge\TolerantParser\TolerantCompletorTestCase;
+use Generator;
+use Phpactor\Completion\Bridge\TolerantParser\WorseReflection\WorseLocalVariableCompletor;
+use Phpactor\TextDocument\TextDocument;
+use Phpactor\WorseReflection\ReflectorBuilder;
+
+class WorseNamedParameterCompletorTest extends TolerantCompletorTestCase
+{
+    protected function createTolerantCompletor(TextDocument $source): TolerantCompletor
+    {
+        $reflector = ReflectorBuilder::create()->addSource($source)->build();
+        return new WorseNamedParameterCompletor($reflector, $this->formatter());
+    }
+
+    /**
+     * @dataProvider provideComplete
+     */
+    public function testComplete(string $source, array $expected): void
+    {
+        $this->assertComplete($source, $expected);
+    }
+
+    public function provideComplete(): Generator
+    {
+        yield 'Variable' => [
+            '<?php $<>', []
+        ];
+
+        yield 'Constructor' => [
+            '<?php class A{function __construct(string $one){}} new A(o<>',
+            [
+                [
+                    'type' => Suggestion::TYPE_FIELD,
+                    'name' => 'one: ',
+                    'short_description' => 'string $one',
+                ]
+            ]
+        ];
+
+        yield 'Method' => [
+            '<?php class A{function bee(string $one){}} $a = new A(); $a->bee(o<>',
+            [
+                [
+                    'type' => Suggestion::TYPE_FIELD,
+                    'name' => 'one: ',
+                    'short_description' => 'string $one',
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider provideCouldNotComplete
+     */
+    public function testCouldNotComplete(string $source)
+    {
+        $this->assertCouldNotComplete($source);
+    }
+
+    public function provideCouldNotComplete(): Generator
+    {
+        yield 'empty string' => [ '<?php  <>' ];
+        yield 'function call' => [ '<?php echo<>' ];
+        yield 'variable with space' => [ '<?php $foo <>' ];
+        yield 'static variable' => ['<?php Foobar::$<>'];
+    }
+}
