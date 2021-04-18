9c3aacc59f8c8ee7834d6a57bdb8885d9c9acd56
Michael Weimann <mail@michael-weimann.eu>
2021-04-18 11:16:04 +0200

    add NameSearchResultFunctionSnippetFormatter (#50)
    Signed-off-by: Michael Weimann <mail@michael-weimann.eu>

--- lib/Bridge/TolerantParser/ReferenceFinder/NameSearcherCompletor.php
+++ lib/Bridge/TolerantParser/ReferenceFinder/NameSearcherCompletor.php
@@ -6,11 +6,31 @@ use Generator;
 use Microsoft\PhpParser\Node;
 use Phpactor\Completion\Bridge\TolerantParser\TolerantCompletor;
 use Phpactor\Completion\Core\Completor\NameSearcherCompletor as CoreNameSearcherCompletor;
+use Phpactor\Completion\Core\DocumentPrioritizer\DocumentPrioritizer;
+use Phpactor\Completion\Core\Formatter\NameSearchResultFunctionSnippetFormatter;
+use Phpactor\ReferenceFinder\NameSearcher;
+use Phpactor\ReferenceFinder\Search\NameSearchResult;
 use Phpactor\TextDocument\ByteOffset;
 use Phpactor\TextDocument\TextDocument;
+use Phpactor\TextDocument\TextDocumentUri;
 
 class NameSearcherCompletor extends CoreNameSearcherCompletor implements TolerantCompletor
 {
+    /**
+     * @var NameSearchResultFunctionSnippetFormatter
+     */
+    private $snippetFormatter;
+
+    public function __construct(
+        NameSearcher $nameSearcher,
+        NameSearchResultFunctionSnippetFormatter $snippetFormatter,
+        DocumentPrioritizer $prioritizer = null
+    ) {
+        parent::__construct($nameSearcher, $prioritizer);
+
+        $this->snippetFormatter = $snippetFormatter;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -21,5 +41,21 @@ class NameSearcherCompletor extends CoreNameSearcherCompletor implements Toleran
         yield from $suggestions;
 
         return $suggestions->getReturn();
+    }
+
+    protected function createSuggestionOptions(NameSearchResult $result, ?TextDocumentUri $sourceUri = null): array
+    {
+        $suggestions = parent::createSuggestionOptions($result, $sourceUri);
+
+        if ($this->snippetFormatter->canFormat($result)) {
+            return array_merge(
+                $suggestions,
+                [
+                    'snippet' => $this->snippetFormatter->format($result)
+                ]
+            );
+        }
+
+        return $suggestions;
     }
 }
